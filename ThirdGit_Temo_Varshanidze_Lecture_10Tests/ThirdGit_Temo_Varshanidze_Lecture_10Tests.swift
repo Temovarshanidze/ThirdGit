c3620be61d041235d55c181f09252b9ebb34cea8
Temo <anarogava@anas-MacBook-Air.local>
2025-02-19 16:11:25 +0400

    Third Git

---
+++ ThirdGit_Temo_Varshanidze_Lecture_10Tests/ThirdGit_Temo_Varshanidze_Lecture_10Tests.swift
@@ -0,0 +1,17 @@
+//
+//  ThirdGit_Temo_Varshanidze_Lecture_10Tests.swift
+//  ThirdGit_Temo_Varshanidze_Lecture_10Tests
+//
+//  Created by ana rogava on 19.02.25.
+//
+
+import Testing
+@testable import ThirdGit_Temo_Varshanidze_Lecture_10
+
+struct ThirdGit_Temo_Varshanidze_Lecture_10Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
